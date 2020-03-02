e4b5b6fef0affba0327cde922f0f7dd3cc13e37c
David Knudson <djknudson@icloud.com>
2020-03-02 09:31:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,14 +13,10 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   new_cart = []
   count = 0
 
-  while count < cart.length
+  while counter < cart.length
     new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
     if new_cart_item != nil
       new_cart_item[:count] += 1
